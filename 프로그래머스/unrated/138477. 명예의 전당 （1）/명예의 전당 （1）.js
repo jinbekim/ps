aa769f9d6278ed2692630ae606df73e77155778e
jinbekim <59330110+jinbekim@users.noreply.github.com>
2022-12-01 14:55:59 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,14 +1,12 @@
 function solution(k, score) {
-    var answer = [];
     const hall = [];
-    score.forEach((val) => {
+    return score.reduce((answer, val) => {
         const arrLen = hall.push(val);
         hall.sort((a,b) => b - a);
         if (arrLen > k) {
             hall.pop();
         }
         answer.push(hall[hall.length - 1]);
-    })
-    console.log(answer);
-    return answer;
+        return answer;
+    },[]);
 }
