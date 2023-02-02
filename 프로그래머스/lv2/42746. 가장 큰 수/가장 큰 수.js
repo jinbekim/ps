bbe66b82dc06082b47c55b1426247906225034c1
jinbekim <59330110+jinbekim@users.noreply.github.com>
2023-02-03 00:37:14 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -3,7 +3,7 @@
 // 	return answer[0] === 0 ? '0' : answer;
 // }
 function solution(numbers) {
-    var answer = numbers.map(v=>v+'')
+    var answer = numbers.map(String)
                         .sort((a,b) => (b+a)*1 - (a+b)*1)
                         .join('');
 
