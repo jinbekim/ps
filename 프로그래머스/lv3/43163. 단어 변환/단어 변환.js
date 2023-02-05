fc4968349389bd416e9796d4afdf0ccbac297f64
jinbekim <59330110+jinbekim@users.noreply.github.com>
2023-02-06 01:45:57 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,4 +1,4 @@
-function alphabetDiff(word1, word2) {
+function alphabetDiffCounter(word1, word2) {
     let count = 0;
     for (let i = 0; i < word1.length; i++) {
         if (word1[i] !== word2[i]) count++;
@@ -7,26 +7,26 @@ function alphabetDiff(word1, word2) {
 }
 
 function solution(begin, target, words) {
-    if (words.find(val => val === target) === -1) return 0;
+    if (!words.includes(target)) return 0;
     if (begin === target) return 0;
     if (words.length < 3 || words.length > 50) return 0;
     
     
-    const q = [begin];
-    let count = 0;
+    const roundWords = [begin];
+    let roundNumber = 0;
     
-    while (q.length && count <= words.length) {
-        const c= q.slice();
-        while (c.length) {
-            let w = c.pop();
-            if (w === target) return count;
-            for (let word of words) {
-                if (alphabetDiff(word, w) === 1) {
-                    q.push(word);
+    while (roundWords.length && roundNumber <= words.length) {
+        const targetCandidates = roundWords.slice();
+        while (targetCandidates.length) {
+            let maybeTarget = targetCandidates.pop();
+            if (maybeTarget === target) return roundNumber;
+            for (const word of words) {
+                if (alphabetDiffCounter(word, maybeTarget) === 1) {
+                    roundWords.push(word);
                 }
             }   
         }
-        count += 1;
+        roundNumber += 1;
     }
     return 0;
 }
