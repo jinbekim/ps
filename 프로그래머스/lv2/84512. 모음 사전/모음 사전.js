3db1a44e317c5162b9d2e9c0dbe77d495e67a2c5
jinbekim <59330110+jinbekim@users.noreply.github.com>
2023-02-09 17:22:36 +0900

    [level 2] Title: 모음 사전, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -6,12 +6,6 @@ function solution(word) {
         .replaceAll('O', '3')
         .replaceAll('U', '4');
     const length = word.length;
-    // //5-9, 11-15, 17-21, 23-27 29-33, 35-39
-    // //4, 10, 16, 22, 28
-    // //3, 34, 65, 96, 127,
-    // //2 158 
-    // 5 25 125 625
-    // 1 6 31 156
     const multiConst = [1,6,31,156, 781];
     for (let i = 0; i < length; ++i) {
         if (word[i])
