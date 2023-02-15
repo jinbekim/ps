f87b6255d6e6b990fc8b33311918398264cc6bd4
jinbekim <59330110+jinbekim@users.noreply.github.com>
2023-02-16 04:31:48 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,37 +1,24 @@
 function solution(jobs) {
-  jobs.sort((a, b) => {
-    if (a[0] !== b[0]) return a[0] - b[0]
-    return a[1] - b[1]
-  })
+  jobs.sort(([a,b], [c,d]) =>(a-c || b- d));
   const len = jobs.length
 
-  let working
   let timer = jobs[0][0];
-  let totalTime = 0
+  let totalTime = 0;
 
   const readyQueue = [jobs.shift()]
-  while (readyQueue.length || working || jobs.length) {
+  while (readyQueue.length || jobs.length) {
     while (jobs[0] && jobs[0][0] <= timer) {
       const next = jobs.shift()
-      if (readyQueue.length) {
-        const index = readyQueue.findIndex(([s, d]) => d > next[1])
-        readyQueue.splice(index, 0, next)
-      } else {
-        readyQueue.push(next)
-      }
-    }
-    if (!readyQueue.length)  {
-        timer +=1;
-        continue;
+        readyQueue.push(next);
     }
     readyQueue.sort((a,b) => a[1] - b[1]);
     if (readyQueue.length) {
-        working = readyQueue.shift()
-        if (timer <= working[0]) totalTime += working[1];
-        else totalTime += (timer - working[0]) + working[1];
-        timer += working[1]
-        working = null
-    }
+        let [requestTime, workingTime] = readyQueue.shift()
+        const waiting = (timer - requestTime);
+        totalTime += workingTime;
+        if (waiting > 0) totalTime += waiting;
+        timer += workingTime;
+    } else timer += 1;
   }
   return Math.floor(totalTime / len)
 }
