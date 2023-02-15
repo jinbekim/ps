3e01f8623860a7ca23426c8b9cdbb45feaf72067
jinbekim <59330110+jinbekim@users.noreply.github.com>
2023-02-16 03:38:38 +0900

    [level 3] Title: 입국심사, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -0,0 +1,25 @@
+function solution(n, times) {
+  times.sort((a, b) => a - b);
+
+  let left = 1;
+  let right = times[times.length - 1] * n;
+  let answer = right;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    let cnt = 0;
+    for (const time of times) {
+      cnt += Math.floor(mid / time);
+    }
+
+    if (cnt >= n) {
+      answer = Math.min(answer, mid);
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return answer;
+}
